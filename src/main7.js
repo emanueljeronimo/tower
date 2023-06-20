98b8c251648b92f9a20250b2b33428508ebf3c0d
Carolina Birolo <carobirolo@gmail.com>
2023-06-20 17:02:12 -0300

    another commit

--- src/main7.js
+++ src/main7.js
@@ -1,6 +1,8 @@
 class GameObject extends Phaser.Physics.Arcade.Sprite {
+    list = null;
     constructor(scene, list, x, y, texture, width = 50, height = 50) {
         super(scene, x, y, texture);
+        this.list = list;
         list.add(this);
         scene.add.existing(this);
         scene.physics.add.existing(this);
@@ -14,19 +16,59 @@ class Enemy extends GameObject {
         super(scene, list, x, y, 'enemy', width, height);
         this.health = 100;
     }
+
+    takeDamage(damage) {
+        this.health -= damage;
+        if (this.health <= 0) {
+            this.destroy();
+            this.list.remove(this, true, true);
+        }
+    }
 }
 
 class Tower extends GameObject {
-    constructor(scene, list, x, y, width = 50, height = 50) {
+    target = null;
+    range = 200;
+    bulletList = null;
+    lastFired = 0;
+    constructor(scene, list, bulletList, x, y, width = 50, height = 50) {
         super(scene, list, x, y, 'tower', width, height);
-        this.lastFired = 0;
-        this.rangeCircle = scene.add.circle(this.x, this.y, TOWER_RANGE, 0x0000ff, 0.2).setVisible(false);
+        this.bulletList = bulletList;
+        this.rangeCircle = scene.add.circle(this.x, this.y, this.range, 0x0000ff, 0.2).setVisible(false);
+    }
+
+    setTarget(target) {
+        this.target = target;
+    }
+
+    update(time) {
+
+        if (this.target && this.target.health <= 0) {
+            this.target = null;
+        }
+
+        if (this.target && time > this.lastFired + 500) {
+            if (Phaser.Math.Distance.Between(this.target.x, this.target.y, this.x, this.y) <= this.range) {
+                const angle = Phaser.Math.Angle.Between(this.x, this.y, this.target.x, this.target.y);
+                this.setAngle(Phaser.Math.RAD_TO_DEG * angle); // Set the angle of the tower sprite
+
+                const bullet = new Bullet(this.scene, this.bulletList, this.x, this.y - this.height / 2);
+                bullet.setAngle(Phaser.Math.RAD_TO_DEG * angle); // Set the rotation of the bullet sprite
+                bullet.setVelocityX(Math.cos(angle) * 400);
+                bullet.setVelocityY(Math.sin(angle) * 400);
+            }
+            this.lastFired = time;
+        }
     }
 }
 
 class Bullet extends GameObject {
+    damage = 10;
     constructor(scene, list, x, y, width = 50, height = 50) {
         super(scene, list, x, y, 'bullet', width, height);
+    }
+    getDamage() {
+        return this.damage;
     }
 }
 
@@ -53,10 +95,8 @@ var enemy;
 var enemies;
 var bullets;
 var towers;
-var lastFired = 0;
 var addTowerButton;
 var addTowerMode = false;
-const TOWER_RANGE = 200;
 
 function preload() {
     this.load.image('bullet', 'bullet.png');
@@ -67,68 +107,52 @@ function preload() {
 function create() {
 
     addTowerButton = this.add.text(10, 10, 'Add Tower: OFF', { font: '24px Arial', fill: '#ffffff' }).setInteractive();
-    addTowerButton.on('pointerdown', function() {
+    addTowerButton.on('pointerdown', function () {
         addTowerMode = !addTowerMode;
         addTowerButton.setText('Add Tower: ' + (addTowerMode ? 'ON' : 'OFF'));
     });
-    
+
     enemies = this.physics.add.group();
     enemy = new Enemy(this, enemies);
 
     bullets = this.physics.add.group();
     towers = this.physics.add.group();
-   
 
-    this.physics.add.overlap(enemies, bullets, function(enemy, bullet) {
-        enemy.health -= 10;
+
+    this.physics.add.overlap(enemies, bullets, function (enemy, bullet) {
+        enemy.takeDamage(bullet.damage)
         bullet.destroy();
     }, null, this);
 }
 
 function update(time, delta) {
     var that = this;
-    if (enemy.health <= 0) {
-        enemy.destroy();
-        towers.getChildren().forEach(function(tower) {
-            tower.lastFired = 0;
-        });
-    }
 
     if (addTowerMode && this.input.activePointer.isDown) {
-        new Tower(this,towers, this.input.activePointer.worldX, this.input.activePointer.worldY);
-    }  
+        addTowerMode = false;
+        let tower = new Tower(this, towers, bullets, this.input.activePointer.worldX, this.input.activePointer.worldY);
+        tower.setTarget(enemy);
+    }
 
     // Handle tower clicks
     if (this.input.activePointer.isDown) {
-        var clickedTowers = towers.getChildren().filter(function(tower) {
+        var clickedTowers = towers.getChildren().filter(function (tower) {
             return Phaser.Geom.Rectangle.Contains(tower.getBounds(), this.input.activePointer.worldX, this.input.activePointer.worldY);
         }, this);
 
         if (clickedTowers.length > 0) {
             clickedTowers[0].rangeCircle.setVisible(true);
         } else {
-            towers.getChildren().forEach(function(tower) {
+            towers.getChildren().forEach(function (tower) {
                 tower.rangeCircle.setVisible(false);
             });
         }
     }
 
-    if (enemy && time > lastFired + 500) {
-        towers.getChildren().forEach(function(tower) {
-            if (Phaser.Math.Distance.Between(enemy.x, enemy.y, tower.x, tower.y) <= TOWER_RANGE) {
-                // Calculate the angle between the tower and the enemy
-                var angle = Phaser.Math.Angle.Between(tower.x, tower.y, enemy.x, enemy.y);
-                tower.setAngle(Phaser.Math.RAD_TO_DEG * angle); // Set the angle of the tower sprite
-
-                var bullet = new Bullet(that,bullets, tower.x, tower.y - tower.height / 2);
-                bullet.setAngle(Phaser.Math.RAD_TO_DEG * angle); // Set the rotation of the bullet sprite
-                bullet.setVelocityX(Math.cos(angle) * 400);
-                bullet.setVelocityY(Math.sin(angle) * 400);
-                
-            }
-        });
-
-        lastFired = time;
+    if (enemy) {
+        towers.getChildren().forEach(function (tower) {
+            tower.update(time);
+        });      
     }
 
     // Move the enemy
@@ -137,7 +161,7 @@ function update(time, delta) {
     }
 
     // Remove bullets that have gone out of bounds
-    bullets.getChildren().forEach(function(bullet) {
+    bullets.getChildren().forEach(function (bullet) {
         if (bullet.y < 0 || bullet.y > config.height) {
             bullet.destroy();
         }
