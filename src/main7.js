70556fda7eb3344875fb4dda17b3183b8959e0c4
Carolina Birolo <carobirolo@gmail.com>
2023-07-22 15:13:11 -0300

    other commit

--- src/main7.js
+++ src/main7.js
@@ -96,7 +96,7 @@ class Tower extends GameObject {
 
 class Bullet extends GameObject {
     damage = 10;
-    maxDistance = 500;
+    maxDistance = 150;
 
     constructor(scene, list, x, y, angle, height, width) {
         super(scene, list, x, y, 'bullet', height, width);
@@ -118,52 +118,66 @@ class Bullet extends GameObject {
 
 class MapGenerator {
 
-    static generateMap(game, unitSize, height, width) {
+    static generateMap(game, unitSize, rows, cols) {
 
         var path = []
-        var xpath = (unitSize / 2) * 3;
-        var ypath = height / 2 * unitSize;
+        /*var xpath = (unitSize / 2) * 3;
+        var ypath = rows / 2 * unitSize;
         path.push({ x: 0, y: ypath });
         path.push({ x: (unitSize / 2), y: ypath });
-        for (var i = 3; i < width + 1; i++) {
+        for (var i = 3; i < cols + 1; i++) {
             path.push({ x: xpath, y: ypath });
             xpath = (unitSize * i) - (unitSize / 2);
-        }
+        }*/
 
 
-        const gridSize = { width: 17, height: 15 };
-        const cellSize = { width: unitSize, height: unitSize };
-
-
-        /* this could be good if I want a background
-        const grid = this.add.image(0, 0, 'grid');
-        grid.setOrigin(0);
-        */
+        const gridSize = { cols: cols, rows: rows };
 
         // Create a group to hold the grid cells
         cells = game.add.group();
 
         // Loop through each cell in the grid
-        for (let row = 1; row < gridSize.height; row++) {
-            for (let col = 1; col < gridSize.width; col++) {
-                const x = col * cellSize.width;
-                const y = row * cellSize.height;
+        for (let row = 1; row < gridSize.rows; row++) {
+            for (let col = 1; col < gridSize.cols; col++) {
+                const x = col * unitSize;
+                const y = row * unitSize;
 
                 // Create a cell sprite and add it to the cells group
                 const cell = game.add.sprite(x, y, null);
-                cell.setInteractive(new Phaser.Geom.Rectangle(0, 0, cellSize.width, cellSize.height), Phaser.Geom.Rectangle.Contains);
+                cell.setInteractive(new Phaser.Geom.Rectangle(0, 0, unitSize, unitSize), Phaser.Geom.Rectangle.Contains);
+                cell.setSize(unitSize, unitSize);
+                cell.setDisplaySize(unitSize, unitSize);
                 cells.add(cell);
             }
         }
 
-        // Set the position of the grid based on the cell size
-        const offsetX = (game.cameras.main.width - (gridSize.width * cellSize.width)) / 2;
-        const offsetY = (game.cameras.main.height - (gridSize.height * cellSize.height)) / 2;
-        cells.children.iterate(function (cell) {
-            cell.x += offsetX;
-            cell.y += offsetY;
-        });
+        let cell0 = cells.getChildren()[0];
 
+        path.push({ x: (unitSize * (cols - 1)) + cell0.x + 1, y: (unitSize / 2 * (rows)) + cell0.y - (unitSize / 2) + 1 });
+        path.push({ x: path[0].x - unitSize, y: path[0].y });
+
+        const LEFT = "LEFT", UP = "UP", DOWN = "DOWN";
+        const directions = [LEFT, UP, DOWN];
+
+        let leftF = ({ x, y }) => ({ x: x - unitSize, y });
+        let upF = ({ x, y }) => ({ x, y: y - unitSize });
+        let downF = ({ x, y }) => ({ x, y: y + unitSize });
+
+       let pathConfigThree = {
+            LEFT: [leftF, upF, downF],
+            UP: [upF, leftF],
+            DOWN: [downF, leftF],
+        }
+
+        while(path[path.length-1].x > cell0.x) {
+            let nextDirection = directions[Math.floor(Math.random() * directions.length)];
+            let arrF = pathConfigThree[nextDirection];
+            let {x,y} = arrF[Math.floor(Math.random() * arrF.length)]({x: path[path.length-1].x, y:path[path.length-1].y});
+            if(y > cell0.y && y < (cell0.y * rows)){
+                path.push({x, y});
+            }
+        }
+        
         // Remove sprites that touch the specified points
         path.forEach((point) => {
             cells.children.each((cell) => {
@@ -197,8 +211,8 @@ var config = {
     },
     unitSize: 20,
     grid: {
-        height: 15,
-        width: 17
+        rows: 15,
+        cols: 17
     }
 };
 
@@ -219,7 +233,7 @@ function preload() {
 
 function create() {
 
-    let path = MapGenerator.generateMap(this, config.unitSize, config.grid.height, config.grid.width);
+    let path = MapGenerator.generateMap(this, config.unitSize, config.grid.rows, config.grid.cols);
 
     enemies = this.physics.add.group();
     enemy = new Enemy(this, enemies, path, path[0].x, path[0].y, config.unitSize, config.unitSize);
