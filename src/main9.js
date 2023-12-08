d2533c06e5dc46d98f6af2769a4183ed83e205f8
Carolina Birolo <carobirolo@gmail.com>
2023-12-08 12:13:19 -0300

    another commit

--- src/main9.js
+++ src/main9.js
@@ -623,7 +623,7 @@ class Game extends Phaser.Scene {
   preload() {
     this.load.image('main-tower', 'assets/main-tower.png');
     this.load.image('bullet', 'assets/bullet.png');
-    this.load.image('enemy', 'assets/enemy-4.png');
+    this.load.image('enemy', 'assets/enemy-6.png');
     this.load.image('tower', 'assets/tower.png');
     this.load.image('particle', 'assets/particle.png');
   }
